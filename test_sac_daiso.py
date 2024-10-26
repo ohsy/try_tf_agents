43caa25765f2f5290ec6cc1cc3726babb159fbda
ohsy <neocella@gmail.com>
2024-10-26 08:34:38 +0000

    env_daiso fixed

--- test_sac_daiso.py
+++ test_sac_daiso.py
@@ -15,6 +15,7 @@ import numpy as np
 # import pyvirtualdisplay
 import reverb
 import time
+from datetime import datetime
 
 import tensorflow as tf
 
@@ -33,6 +34,7 @@ from tf_agents.drivers import dynamic_step_driver
 from tf_agents.drivers import dynamic_episode_driver
 from tf_agents.eval import metric_utils
 from tf_agents.metrics import tf_metrics
+from tf_agents.policies import actor_policy
 from tf_agents.policies import greedy_policy
 from tf_agents.policies import py_tf_eager_policy
 from tf_agents.policies import random_tf_policy
@@ -41,7 +43,7 @@ from tf_agents.replay_buffers import reverb_utils
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
 from tf_agents.trajectories import trajectory
 from tf_agents.specs import tensor_spec
-from tf_agents.utils import common
+from tf_agents.utils import common, tensor_normalizer
 from tf_agents.train.utils import spec_utils
 from tf_agents.train.utils import train_utils
 from tf_agents.policies import policy_saver
@@ -49,6 +51,19 @@ from tf_agents.policies import policy_saver
 # to suppress warning of data_manager.py
 import warnings
 warnings.filterwarnings("ignore")
+
+is_gpu_used = True
+if is_gpu_used:
+    gpus = tf.config.list_physical_devices('GPU')
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except:
+        sys.exit(f"tf.config.experimental.set_memory_growth() is not working for {gpus}")
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
 # def fxn():
 #     warnings.warn("deprecated", DeprecationWarning)
 #     warnings.warn("deprecated", FutureWarning)
@@ -69,9 +84,9 @@ env_name = "DaisoSokcho" # @param {type:"string"}
 # num_iterations = 100000 # 20000 # @param {type:"integer"}
 num_iterations = 5000 # 20000 # @param {type:"integer"}
 
-initial_collect_steps = 10000  # @param {type:"integer"}
+initial_collect_steps = 1000  # @param {type:"integer"}
 collect_steps_per_iteration = 1# @param {type:"integer"}
-replay_buffer_max_length = 10000  # @param {type:"integer"}
+replay_buffer_max_length = 5000  # @param {type:"integer"}
 
 batch_size = 8  # @param {type:"integer"}
 critic_learning_rate = 6e-4  # @param {type:"number"}
@@ -85,7 +100,7 @@ reward_scale_factor = 1.0 # @param {type:"number"}
 actor_fc_layer_params = (256, 256)
 critic_joint_fc_layer_params = (256, 256)
 
-log_interval = 200 # @param {type:"integer"}
+log_interval = 1000 # @param {type:"integer"}
 
 num_eval_episodes = 10 # @param {type:"integer"}
 eval_interval = 1000 # @param {type:"integer"}
@@ -133,6 +148,14 @@ actor_net = actor_distribution_network.ActorDistributionNetwork(
 
 train_step = train_utils.create_train_step()
 
+class NormalizedActorPolicy(actor_policy.ActorPolicy):
+    def __init__(self, *args, **kwargs):
+        super(NormalizedActorPolicy, self).__init__(
+            *args,
+            # observation_normalizer=tensor_normalizer.EMATensorNormalizer(observation_spec),
+            observation_normalizer=tensor_normalizer.StreamingTensorNormalizer(observation_spec),
+            **kwargs
+        )
 agent = sac_agent.SacAgent(
       time_step_spec,
       action_spec,
@@ -144,6 +167,7 @@ agent = sac_agent.SacAgent(
           learning_rate=critic_learning_rate),
       alpha_optimizer=tf.keras.optimizers.Adam(
           learning_rate=alpha_learning_rate),
+      actor_policy_ctor=NormalizedActorPolicy,
       target_update_tau=target_update_tau,
       target_update_period=target_update_period,
       td_errors_loss_fn=tf.math.squared_difference,
@@ -237,6 +261,8 @@ print(f"dataset.cardinality()={dataset.cardinality()}", flush=True)
 print(f"replay_buffer.capacity={replay_buffer.capacity}", flush=True)
 print(f"replay_buffer.num_frames()={replay_buffer.num_frames()}", flush=True)
 
+all_before = time.time()
+
 init_collect_driver = dynamic_step_driver.DynamicStepDriver(
     train_env,
     random_policy,
@@ -290,6 +316,13 @@ avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
 print(f"before for-loop, avg_return={avg_return}", flush=True)
 returns = [avg_return]
 
+dt = datetime.now().strftime('%m%d_%H%M')
+summary_path = os.path.join(
+    os.path.abspath(os.getcwd()),
+    f'log/summary_{dt}'
+)
+summary_writer = tf.summary.create_file_writer(summary_path)
+
 # Reset the environment.
 # time_step = train_env.reset()
 time_step = train_py_env.reset()
@@ -304,30 +337,31 @@ for _ in range(num_iterations):
     # Sample a batch of data from the buffer and update the agent's network.
     experience, unused_info = next(iterator)
 
-    print(f"experience=")
-    print(f"{experience}", flush=True)
+    # print(f"experience=")
+    # print(f"{experience}", flush=True)
 
-    loss_info = agent.train(experience)
-    train_loss = loss_info.loss
+    with summary_writer.as_default():
+        loss_info = agent.train(experience)
+        train_loss = loss_info.loss
 
     step = agent.train_step_counter.numpy()
 
     if step % log_interval == 0:
-        print('step = {0}: loss = {1}'.format(step, train_loss))
         after = time.time()
-        print('time used = {0}'.format(after - before))
+        print(f'step = {step}: loss = {train_loss}, time used = {after - before}', flush=True)
         before = after
 
     if step % eval_interval == 0:
         avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
-        print('step = {0}: Average Return = {1}'.format(step, avg_return))
+        print(f'step = {step}: average return = {avg_return}', flush=True)
         returns.append(avg_return)
 
-# print(dir(agent.policy))
+all_after = time.time()
+print(f"total time = {all_after-all_before}")
 
 checkpoint_path = os.path.join(
     os.path.abspath(os.getcwd()),
-    f'result/checkpoint'
+    f'result/checkpoint_{dt}'
 )
 train_checkpointer = common.Checkpointer(
     ckpt_dir=checkpoint_path,
