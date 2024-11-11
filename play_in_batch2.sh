80f10d67db1756ce8d18a363376cc37032328017
yoh <neocella@naver.com>
2024-11-11 21:48:12 +0900

    play_mini added and doing experiments

--- play_in_batch2.sh
+++ play_in_batch2.sh
@@ -16,12 +16,18 @@
 # python3 play.py -e DaisoSokcho_discrete -a DQN_multiagent -n 7 &> o_DaisoSokcho_discrete_DQN_multiagent_num_actions_7_2 &
 
 
-python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 100 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_100_2 ; \
-python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 500 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_500_2 ; \
-python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 1000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_1000_2 ; \
-python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 5000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_5000_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 100 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_100_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 500 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_500_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 1000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_1000_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 2000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_2000_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 3000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_3000_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 4000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_4000_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 5000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_5000_2 ; \
+python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 6000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_6000_2 ; \
+python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 7000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_7000_2 ; \
+python3 play_mini.py -e Reacher-v2_discrete -a CDQN_multiagent -n 3 -i 8000 &> o_Reacher-v2_discrete_CDQN_multiagent_num_actions_3_num_init_collect_steps_8000_2 ; \
 
-python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 100 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_100_2 ; \
-python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 500 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_500_2 ; \
-python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 1000 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_1000_2 ; \
-python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 5000 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_5000_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 100 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_100_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 500 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_500_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 1000 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_1000_2 ; \
+# python3 play_mini.py -e Reacher-v2_discrete -a DQN_multiagent -n 3 -i 5000 &> o_Reacher-v2_discrete_DQN_multiagent_num_actions_3_num_init_collect_steps_5000_2 ; \
