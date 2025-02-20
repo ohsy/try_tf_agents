8548a15c2178feb764ec3a262b6ad0ea30af3d0c
ohsy <neocella@naver.com>
2025-02-20 07:12:28 +0000

    script removed

--- script/DaisoSokcho_discrete_CDQN_multiagent_reverb/playbatch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./playbatch0.sh &
-./playbatch1.sh &
-./playbatch2.sh &
-./playbatch3.sh &
-./playbatch4.sh &
-
--- script/Pendulum-v1_SAC_wo_normalize/change.sh
+++
@@ -1,9 +0,0 @@
-
-mv play_in_batch.sh playbatch.sh
-mv play_in_batch0.sh playbatch0.sh
-mv play_in_batch1.sh playbatch1.sh
-mv play_in_batch2.sh playbatch2.sh
-mv play_in_batch3.sh playbatch3.sh
-mv play_in_batch4.sh playbatch4.sh
-mv post_process.sh postprocess.sh
-
